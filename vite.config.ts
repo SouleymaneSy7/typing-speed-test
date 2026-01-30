4c782b0d926a6e42b01179a2e33883b74e0b59c2
souleymanesy7 <souleymanesyservices@gmail.com>
2026-01-30 14:56:33 +0000

    chore(vite config): update the vite configs for tailwindcss and server handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
+
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  },
+});
