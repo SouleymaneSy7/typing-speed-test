532a3dd3e4eef7a8193c0a49d23a42e8ca431def
souleymanesy7 <souleymanesyservices@gmail.com>
2026-02-03 20:52:54 +0000

    feat: update the global css style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { RefreshCcw } from "lucide-react";
+import { Logo } from "./components/shared/logo";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Vite + React + Tailwind v4!
-      </h1>
+    <div className="flex min-h-screen items-center justify-center">
+      <div>
+        <div className="my-5 flex flex-col gap-4">
+          <Logo />
+        </div>
+
+        <div className="my-5 flex flex-col gap-4">
+          <button className="btn-primary">Primary Button</button>
+          <button className="btn-secondary">
+            Secondary Button
+            <RefreshCcw />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
