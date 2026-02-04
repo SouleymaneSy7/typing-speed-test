2e4211df7b5a2f25841c74588c0b0621c1b7b2dd
souleymanesy7 <souleymanesyservices@gmail.com>
2026-02-04 21:01:21 +0000

    feat: add the header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import { RefreshCcw } from "lucide-react";
-import { Logo } from "./components/shared/logo";
+import Header from "./components/layout/header";
 
 function App() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div>
-        <div className="my-5 flex flex-col gap-4">
-          <Logo />
-        </div>
-
-        <div className="my-5 flex flex-col gap-4">
-          <button className="btn-primary">Primary Button</button>
-          <button className="btn-secondary">
-            Secondary Button
-            <RefreshCcw />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
