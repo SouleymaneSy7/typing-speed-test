51ae77174692a9ad0f3e1b1c6542a3ae6b8e3cb1
souleymanesy7 <souleymanesyservices@gmail.com>
2026-02-04 21:07:28 +0000

    feat(components/layout): update the header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,17 +1,17 @@
-import { useIsMobile } from "@/hooks/use-mobile";
 import Container from "../common/container";
-import { PersonalBestIcon } from "../icons/icons.component";
+
 import { Logo } from "../shared/logo";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { PersonalBestIcon } from "../icons/icons.component";
 
 const Header = () => {
   const bestScore = 93;
-
   const isMobile = useIsMobile(550);
 
   return (
     <Container
       as={"header"}
-      className="mt-space-200 mb-space-400 md:mb-space-500 lg:mb-space-800 md:mt-space-400 gap-space-200 container flex items-center justify-between"
+      className="pt-space-200 pb-space-400 md:pb-space-500 lg:pb-space-800 md:pt-space-400 gap-space-200 container flex items-center justify-between"
     >
       <a href="#">
         <Logo />
